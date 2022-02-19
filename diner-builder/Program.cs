e6f24b71546ed0aad4bd14d3235ec30d948c56d8
joshuamccluskey <jpiff57@gmail.com>
2022-02-19 09:27:10 -0800

    first commit

---
+++ diner-builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diner_builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
